5dbd651b60e775d4959fe73053a02dd7a4c4af76
A K ANISH JOHN <akanishjohn37@gmail.com>
2025-11-30 08:30:47 +0530

    connect with database supabase

--- queue_backend/queue_backend/settings.py
+++ queue_backend/queue_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,11 +91,14 @@ CORS_ALLOW_ALL_ORIGINS = True # Allow all for now to simplify deployment
 
 
 # Use SQLite for development
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Ensure default user model (explicit)
